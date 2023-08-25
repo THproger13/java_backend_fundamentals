123bb4b517a9c105e69081ab4d097732d0af0051
THproger13 <thphysics@naver.com>
2023-08-25 10:51:57 +0900

    회원정보 수정, 회원탈퇴

--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -5,41 +5,48 @@ import service.MemberService;
 import java.util.Scanner;
 
 public class MainClass {
+
     // 스캐너를 이용하여
     // 1. 회원가입
     // 2. 로그인
     // 3. 회원 목록 조회
     // 4. 회원 정보 수정
-    // 5. 회원 삭제
+    // 5. 회원 탈퇴
+    // 6. 로그아웃
     // 0. 종료
     // 를 선택하는 코드를 작성합시다.
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         MemberService memberService = new MemberService();
-        boolean run = true;
-        while (run) {
+
+        //boolean run = true;
+        while (true) {
             System.out.println("====== 회원관리 ======");
-            System.out.println("1.회원 가입 2.로그인 3.회원 목록 조회 4.회원 정보 수정 5.회원 삭제 0.종료");
+            System.out.println("1.회원 가입 2.로그인 3.회원 목록 조회 4.회원 정보 수정 5.회원 탈퇴 6. 로그 아웃 0.종료");
             System.out.println("선택>  ");
             int sel = scanner.nextInt();
             if (sel == 1) {
                 System.out.println("회원 가입 메뉴");
-                memberService.save();
+                memberService.signin();
             } else if (sel == 2) {
                 System.out.println("로그인");
-                memberService.findById();
+                memberService.login();
             } else if (sel == 3) {
                 System.out.println("회원 목록 조회");
                 memberService.findAll();
             } else if (sel == 4) {
                 System.out.println("회원 정보 수정");
-                //memberService.update();
+                memberService.update();
             } else if (sel == 5) {
-                System.out.println("회원 삭제");
-//              memberService.delete();
+                System.out.println("회원 탈퇴");
+                memberService.delete();
+            } else if(sel == 6){
+                System.out.println("로그 아웃");
+                memberService.logout();
             } else if (sel == 0) {
                 System.out.println("종료");
-                run = false;
+
+                break;
             }
         }
     }
--- src/service/MemberService.java
+++ src/service/MemberService.java
@@ -11,9 +11,11 @@ public class MemberService {
 
     Scanner scanner = new Scanner(System.in);
     MemberRepository memberRepository = new MemberRepository();
-    MemberDTO memberDTO = new MemberDTO();
     private List<MemberDTO> memberDTOList = new ArrayList<>();
+
+    //로그인 여부를 판단하는 static 필드
     private static String loginEmail = null;
+    boolean islogin = false;
     /*
     회원가입
 
@@ -21,49 +23,95 @@ public class MemberService {
     관리번호(id)는 회원가입시 자동으로 부여됨
 
     */
-    public void save(){
-        System.out.println("회원 가입할 이름 입력 : ");
-        String membername = scanner.next();
+    public void signin(){
         System.out.println("회원 가입할 이메일 입력 : ");
-        String memberemail = scanner.next();
-        System.out.println("회원 가입할 비밀 번호 입력 : ");
-        String memberpassword = scanner.next();
-        System.out.println("회원 가입할 전화 번호 입력 : ");
-        String memberMobile = scanner.next();
+        String memberEmail = scanner.next();
+        boolean isduplicateemail = MemberRepository.isduplicateemail(memberEmail);
+        if(isduplicateemail){
+            System.out.println("사용 중인 이메일 입니다.");
+        }else {
+            System.out.println("사용 가능한 이메일 입니다. ");
+            System.out.println("회원 가입할 비밀 번호 입력 : ");
+            String memberPassword = scanner.next();
+            System.out.println("회원 가입할 이름 입력 : ");
+            String memberName = scanner.next();
+            System.out.println("회원 가입할 전화 번호 입력 : ");
+            String memberMobile = scanner.next();
 
-        MemberDTO memberDTO = new MemberDTO(membername, memberemail, memberpassword, memberMobile);
+            MemberDTO memberDTO = new MemberDTO(memberName, memberEmail, memberPassword, memberMobile);
 
-        boolean result = MemberRepository.save(memberDTO);
-        if (result) {
-            System.out.println("회원가입 성공");
-            System.out.println(memberDTO.toString());
-            //System.out.println(memberRepository.findAll());
-        } else {
-            System.out.println("회원가입 실패");
+            boolean result = MemberRepository.signin(memberDTO);
+            System.out.println(result ? "회원가입 성공" : "회원가입 실패");
+
         }
 
     }
-//로그인 기능
-    public void findById() {
+
+    //회원 전체 목록
+    public void findAll() {
+        List<MemberDTO> members = memberRepository.findAll();
+        for (MemberDTO member : members) {
+            System.out.println(member.toString());
+        }
+    }
+
+    //회원 전화 번호 수정
+    public void update(){
+        System.out.println("이메일을 입력해 주세요! : ");
+        String confirmEmail = scanner.next();
+        System.out.println("비밀 번호를 입력해 주세요! : ");
+        String confirmPassword = scanner.next();
+        if(memberRepository.confirmmember(confirmEmail,confirmPassword)){
+            System.out.println(confirmEmail + "님 확인 되었습니다.");
+            System.out.println("수정할 전화 번호를 입력해 주세요! : ");
+            String updatedMobile = scanner.next();
+            if(memberRepository.update(updatedMobile,confirmEmail)) {
+                System.out.println(confirmEmail + "님 전화 번호가 수정 되었습니다.");
+                //System.out.println(memeberDTO.toString());
+            }else{
+                System.out.println(confirmEmail + "님 전화 번호 수정을 실패하였습니다.");
+            }
+            //loginEmail = confirmEmail;
+        }else {
+            System.out.println("회원 확인 실패");
+        }
+    }
+    //회원 탈퇴
+    public void delete() {
+        if(!loginEmail.isEmpty()){
+            System.out.println("탈퇴 전 비밀 번호를 입력해 주세요! : ");
+            String confirmDeletePassword = scanner.next();
+            if(memberRepository.confirmDeletePassword(confirmDeletePassword)){
+                memberRepository.delete(confirmDeletePassword);
+            }else{
+                System.out.println("비밀번호가 틀렸습니다.");
+            }
+        }else{
+            System.out.println("로그인을 먼저 해주세요!");
+        }
+    }
+
+    //로그인 기능
+    public void login() {
         System.out.println("로그인 할 이메일을 입력해 주세요! : ");
-        String memberemail = scanner.next();
+        String memberEmail = scanner.next();
         System.out.println("로그인 할 비밀 번호를 입력해 주세요! : ");
-        String memberpassword = scanner.next();
-        if(memberRepository.findById(memberemail,memberpassword)){
-            loginEmail = memberemail;
+        String memberPassword = scanner.next();
+        if(memberRepository.login(memberEmail,memberPassword)){
             System.out.println("로그인 성공");
+            loginEmail = memberEmail;
         }else {
             System.out.println("로그인 실패");
         }
     }
-//회원 전체 목록을 출력
-    public void findAll() {
 
-        memberRepository.findAll();
-
+    //로그 아웃
+    public void logout() {
+        if(memberRepository.logout()) {
+            System.out.println("로그 아웃 되었습니다.");
+            islogin = false;
+       }
     }
 
-    //회원 정보 수정
 
-    //회원 삭제
 }
