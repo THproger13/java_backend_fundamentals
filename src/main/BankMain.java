0f0f8c4b843ee04a65e4e9297b9557fabfd6a7b8
THproger13 <thphysics@naver.com>
2023-08-29 16:14:59 +0900

    완료

--- src/main/BankMain.java
+++ src/main/BankMain.java
@@ -1,21 +1,26 @@
 package main;
+
+import repository.BankRepository;
 import service.BankService;
 
 import java.util.Scanner;
 
+
 public class BankMain {
     public static void main(String[] args) {
+        BankRepository bankRepository = new BankRepository();
+
         BankService bankService = new BankService();
         Scanner sc = new Scanner(System.in);
-        boolean run = true;
-        while(run) {
+        boolean runMainProgram = true;
+        while (runMainProgram) {
             System.out.println("======은행 프로젝트=======\n");
             System.out.println("1. 신규 계좌 등록 메뉴 2. 잔액 조회 메뉴 3. 입금 메뉴" +
-                    " 4. 출금 메뉴 5. 입출금 내역 조회 메뉴 6. 고객 정보 샘플 데이터 저장 7. 거래 내역 샘플 데이터 저장 0. 은행 프로그램 종료\n");
+                    " 4. 출금 메뉴 5. 입출금 내역 조회 메뉴 0. 은행 프로그램 종료\n");
             System.out.println("=================\n");
             System.out.println("메뉴를 입력해 주세요 : \n");
-            int menu = sc.nextInt();
-            switch (menu){
+            int menuMain = sc.nextInt();
+            switch (menuMain) {
                 // 신규 계좌 등록 메뉴
                 case 1:
                     System.out.println("신규 계좌 등록 메뉴");
@@ -30,37 +35,67 @@ public class BankMain {
                 //입금 메뉴
                 case 3:
                     System.out.println("입금 메뉴");
-                    //boardService.check();
+                    bankService.deposit();
                     break;
 
                 //출금 메뉴
                 case 4:
                     System.out.println("출금 메뉴");
-                    //boardService.update();
+                    bankService.withdraw();
+                    break;
+                //게시판 종료
+                case 0:
+                    System.out.println("게시판을 종료합니다.");
+                    runMainProgram = false;
                     break;
 
                 //입출금 내역 조회 메뉴
                 case 5:
-                    System.out.println("입출금 내역 조회 메뉴");
-                    //boardService.delete();
-                    break;
-                    //샘플 데이터 저장
-                case 6:
-                    System.out.println("고객정보 샘플데이터 저장");
-                    //boardService.saveclientsample();
-                    break;
+                    boolean runDepositWithdrawProgram = true;
 
-                case 7:
-                    System.out.println("거래내역 샘플데이터 저장");
-                    //boardService.saveaccountsample();
-                    break;
+                    while (runDepositWithdrawProgram) {
+                        System.out.println("입출금 내역 조회 메뉴");
+                        System.out.println("1. 계좌 전체 내역 메뉴 2. 입금 내역 메뉴 3. 출금 내역 메뉴" +
+                                "4. 입출금 내역 조회 프로그램 종료\n");
+                        System.out.println("=================\n");
+                        System.out.println("출금하려는 계좌를 입력해주세요! : ");
+                        String accountNumber = sc.next();
 
-                //게시판 종료
-                case 0:
-                    System.out.println("게시판을 종료합니다.");
-                    run = false;
+                        boolean checkaccountresult = bankRepository.cheakaccount(accountNumber);
+                        if (checkaccountresult) {
+                            System.out.println("메뉴를 입력해 주세요 : \n");
+                            int DepositWithdrawProgram = sc.nextInt();
+
+                            switch (DepositWithdrawProgram) {
+                                // 계좌 전체 내역 메뉴
+                                case 1:
+                                    System.out.println("계좌 전체 내역 메뉴");
+                                    bankService.showAccountHistory();
+                                    break;
+                                //입금 내역 메뉴
+                                case 2:
+                                    System.out.println("입금 내역 메뉴");
+                                    bankService.showDepositHistory(accountNumber); // 수정 필요
+                                    break;
+
+                                //출금 내역 메뉴
+                                case 3:
+                                    System.out.println("출금 내역 메뉴");
+                                    bankService.showWithdrawHistory(accountNumber); // 수정 필요
+                                    break;
+
+                                //입출금 내역 프로그램 종료
+                                case 4:
+                                    System.out.println("입출금 내역 프로그램을 종료합니다.");
+                                    runDepositWithdrawProgram = false;
+                                    break;
+                            }
+                        } else {
+                            System.out.println("없는 계좌 입니다.");
+                        }
+                    }
             }
-
         }
     }
 }
+
--- src/repository/BankRepository.java
+++ src/repository/BankRepository.java
@@ -1,16 +1,20 @@
 package repository;
 
 import dto.ClientDTO;
+import dto.AccountDTO;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
 public class BankRepository {
-    List<ClientDTO> clientDTOList = new ArrayList<>();
-    //List<AccountDTO> accountDTOList = new ArrayList<>();
 
-    //계좌 생성시 계좌번호 중복 체크
-    public boolean duplicateaccount(String accountNumber) {
+    List<ClientDTO> clientDTOList = new ArrayList<>();
+    List<AccountDTO> accountDTOList = new ArrayList<>();
+
+    //계좌번호 중복 체크(여러 기능에서 중복 사용)
+    public boolean cheakaccount(String accountNumber) {
         for(ClientDTO clientDTO : clientDTOList){
             if(accountNumber.equals(clientDTO.getAccountNumber())){
                 return true;
@@ -33,4 +37,73 @@ public class BankRepository {
         return null;
     }
 
+    //특정 계좌에 입금하기
+    public void deposit(long deposit, String accountNumber, String bankingAt) {
+        //System.out.println("deposit = " + deposit + ", accountNumber = " + accountNumber + ", bankingAt = " + bankingAt);
+        for(ClientDTO clientDTO : clientDTOList) {
+            if (accountNumber.equals(clientDTO.getAccountNumber())) {
+                //System.out.println(clientDTO.getBalance());
+
+                clientDTO.setBalance(clientDTO.getBalance() + deposit) ;
+                //System.out.println(clientDTO.getBalance());
+
+                AccountDTO accountDTO = new AccountDTO();
+                accountDTO.setDeposit(deposit);
+                accountDTO.setAccountNumber(accountNumber);
+                accountDTO.setBankingAt(bankingAt);
+                accountDTOList.add(accountDTO);
+            }
+        }
+    }
+
+    public boolean checkclientpass(int clientPass) {
+        for(ClientDTO clientDTO : clientDTOList){
+            if(clientPass == clientDTO.getClientPass()){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void withdraw(long withdraw, String accountNumber, String bankingAt) {
+        for(ClientDTO clientDTO : clientDTOList) {
+            if (accountNumber.equals(clientDTO.getAccountNumber())) {
+                //System.out.println(clientDTO.getBalance());
+                if(clientDTO.getBalance() >= withdraw) {
+                    clientDTO.setBalance(clientDTO.getBalance() - withdraw);
+                    //System.out.println(clientDTO.getBalance());
+                    AccountDTO accountDTO = new AccountDTO();
+                    accountDTO.setWithdraw(withdraw);
+                    accountDTO.setAccountNumber(accountNumber);
+                    accountDTO.setBankingAt(bankingAt);
+                    System.out.println(accountNumber + "에서 " + withdraw + "원이 출금되었습니다. ");
+                    accountDTOList.add(accountDTO);
+                }else{
+                    System.out.println("잔액이 부족합니다.");
+                }
+            }
+        }
+    }
+    public List<AccountDTO> showAccountHistory(){
+        return new ArrayList<>(accountDTOList);
+    }
+
+    public List<AccountDTO> showDepositHistory() {
+        for(AccountDTO accountDTO : accountDTOList) {
+            if(accountDTO.getWithdraw() == 0) {
+                return new ArrayList<>(accountDTOList);
+            }
+        }
+        return null;
+
+    }
+
+    public List<AccountDTO> showWithdrawHistory() {
+        for(AccountDTO accountDTO : accountDTOList) {
+            if(accountDTO.getDeposit() == 0) {
+                return new ArrayList<>(accountDTOList);
+            }
+        }
+        return null;
+    }
 }
